c072402c487df8b7d22a2059ef5650d7e7e95579
Andrew Bekhiet <40571928+Andrew-Bekhiet@users.noreply.github.com>
2022-01-14 20:59:30 +0200

    fix: export order_options.dart

--- lib/src/models/meta.dart
+++ lib/src/models/meta.dart
@@ -1,6 +1,7 @@
 export 'meta/audit_record.dart';
 export 'meta/last_edit.dart';
 export 'meta/notification_setting.dart';
+export 'meta/order_options.dart';
 export 'meta/permissions_set.dart';
 export 'meta/property_metadata.dart';
 export 'meta/property_metadata.dart';
