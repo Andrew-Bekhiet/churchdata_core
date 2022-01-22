907b48b62e3b04b74d7cc79a291192ba12111bc5
Andrew Bekhiet <40571928+Andrew-Bekhiet@users.noreply.github.com>
2022-01-22 22:18:47 +0200

    feat: add AuditRecord.fromJsonDoc

--- lib/src/models/meta/audit_record.dart
+++ lib/src/models/meta/audit_record.dart
@@ -4,11 +4,23 @@ import 'package:flutter/foundation.dart';
 
 @immutable
 class AuditRecord extends DocumentObject {
+  static Future<List<AuditRecord>> getAllFromRef(JsonCollectionRef ref) async {
+    return (await ref.orderBy('Time', descending: true).limit(1000).get())
+        .docs
+        .map(AuditRecord.fromJsonDoc)
+        .toList();
+  }
+
   const AuditRecord({
     required JsonRef ref,
     required this.time,
     required this.by,
   }) : super(ref);
+
+  AuditRecord.fromJsonDoc(JsonDoc doc)
+      : by = doc.data()?['By'],
+        time = (doc.data()!['Time'] as Timestamp).toDate(),
+        super.fromJsonDoc(doc);
 
   final DateTime time;
   final String? by;
