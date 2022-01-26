e16ba13848ba2cc968396d34fc479f54366f6dd2
Andrew Bekhiet <40571928+Andrew-Bekhiet@users.noreply.github.com>
2022-01-26 18:41:25 +0200

    fix: export ShareService

--- lib/src/services.dart
+++ lib/src/services.dart
@@ -2,3 +2,4 @@ export 'services/functions_service.dart';
 export 'services/launcher_service.dart';
 export 'services/logging_service.dart';
 export 'services/notifications_service.dart';
+export 'services/share_service.dart';
