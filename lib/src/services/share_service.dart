4759a3ea2410941ad6e9e82dac1ad5bbb455d353
Andrew Bekhiet <40571928+Andrew-Bekhiet@users.noreply.github.com>
2022-01-31 18:26:29 +0200

    fix: shareObject

--- lib/src/services/share_service.dart
+++ lib/src/services/share_service.dart
@@ -80,19 +80,16 @@ class ShareService {
   }
 
   Future<Uri> shareObject<T>(T object) async {
-    switch (T) {
-      case PersonBase:
-        return sharePerson(object as PersonBase);
-      case UserBase:
-        return shareUser(object as UserBase);
-      case QueryInfo:
-        return shareQuery(object as QueryInfo);
-      default:
-        throw UnimplementedError(
-          'Expected an object of type PersonBase, UserBase or QuerInfo, but instead got type' +
-              object.runtimeType.toString(),
-        );
-    }
+    if (object is PersonBase)
+      return sharePerson(object);
+    else if (object is UserBase)
+      return shareUser(object);
+    else if (object is QueryInfo) return shareQuery(object);
+
+    throw UnimplementedError(
+      'Expected an object of type PersonBase, UserBase or QuerInfo, but instead got type' +
+          object.runtimeType.toString(),
+    );
   }
 
   Future<void> shareText(String text) async {
