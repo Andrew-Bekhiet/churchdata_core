9e14d9c9dfba15fbd4fb6beb57bb96d4636cd6d7
Andrew Bekhiet <40571928+Andrew-Bekhiet@users.noreply.github.com>
2022-06-16 23:42:17 +0200

    fix: remove deprecated members

--- lib/src/services/launcher_service.dart
+++ lib/src/services/launcher_service.dart
@@ -3,7 +3,7 @@ import 'package:url_launcher/url_launcher.dart' as l;
 
 class LauncherService {
   Future<bool> launch(String url) async {
-    return l.launch(url);
+    return l.launchUrl(Uri.parse(url));
   }
 
   Future<bool> launchSMSChat(String fomattedPhone) async {
