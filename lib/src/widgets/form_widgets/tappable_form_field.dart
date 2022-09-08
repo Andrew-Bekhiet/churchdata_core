c140d99135bd43423bb13b84e9cc037c65b17dab
Andrew Bekhiet <40571928+Andrew-Bekhiet@users.noreply.github.com>
2022-09-08 22:58:33 +0200

    feat(tappableFormField): handle focus

--- lib/src/widgets/form_widgets/tappable_form_field.dart
+++ lib/src/widgets/form_widgets/tappable_form_field.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class TappableFormField<T> extends StatelessWidget {
+class TappableFormField<T> extends StatefulWidget {
   const TappableFormField({
     super.key,
     required this.initialValue,
@@ -11,41 +13,73 @@ class TappableFormField<T> extends StatelessWidget {
     this.onSaved,
     this.validator,
     this.autovalidateMode,
+    this.focusNode,
   }) : assert(labelText != null || decoration != null);
 
   final T initialValue;
-  final void Function(FormFieldState<T>) onTap;
+  final FutureOr<void> Function(FormFieldState<T>) onTap;
   final String? labelText;
   final Widget? Function(BuildContext, FormFieldState<T>) builder;
   final String? Function(T?)? validator;
   final void Function(T?)? onSaved;
   final InputDecoration Function(BuildContext, FormFieldState<T>)? decoration;
   final AutovalidateMode? autovalidateMode;
+  final FocusNode? focusNode;
+
+  @override
+  State<TappableFormField<T>> createState() => _TappableFormFieldState<T>();
+}
+
+class _TappableFormFieldState<T> extends State<TappableFormField<T>> {
+  late final FocusNode _focusNode = widget.focusNode ??
+      Focus.maybeOf(context) ??
+      FocusNode(debugLabel: 'TappableFormField<$T>:${widget.labelText}');
 
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.symmetric(vertical: 10),
       child: FormField<T>(
-        autovalidateMode: autovalidateMode,
-        initialValue: initialValue,
+        autovalidateMode: widget.autovalidateMode,
+        initialValue: widget.initialValue,
         builder: (state) => InkWell(
-          onTap: () => onTap(state),
-          child: InputDecorator(
-            decoration: decoration != null
-                ? decoration!(context, state)
-                : InputDecoration(
-                    errorText: state.errorText,
-                    labelText: labelText,
-                  ),
-            isEmpty: state.value == null,
-            child: builder(context, state),
+          focusNode: _focusNode,
+          onTap: () async {
+            _focusNode.requestFocus();
+
+            final previousValue = state.value;
+
+            await widget.onTap(state);
+
+            if (state.value != previousValue) _focusNode.nextFocus();
+          },
+          child: AnimatedBuilder(
+            animation: _focusNode,
+            child: widget.builder(context, state),
+            builder: (context, child) => InputDecorator(
+              isFocused: _focusNode.hasFocus,
+              decoration: widget.decoration != null
+                  ? widget.decoration!(context, state)
+                  : InputDecoration(
+                      errorText: state.errorText,
+                      labelText: widget.labelText,
+                    ),
+              isEmpty: state.value == null,
+              child: child,
+            ),
           ),
         ),
-        onSaved: onSaved,
-        validator:
-            validator ?? (value) => value == null ? 'هذا الحقل مطلوب' : null,
+        onSaved: widget.onSaved,
+        validator: widget.validator ??
+            (value) => value == null ? 'هذا الحقل مطلوب' : null,
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    if ((widget.focusNode ?? Focus.maybeOf(context)) == null)
+      _focusNode.dispose();
   }
 }
